16a196901325a8978baef84dd669dcd7a975deb4
lonelysamu <quahjit@gmail.com>
2021-12-08 08:59:27 +0800

    sahootstuff

---
+++ assets/js/questionaire.js
@@ -0,0 +1,62 @@
+var current = 0;
+var cs = 0;
+
+document.querySelector('#contentbody').addEventListener('click', (e) => {
+    if (e.target.classList.contains("questionbegin") == true) {
+
+        document.getElementById("lobby").classList.add("fadetoright");
+        document.querySelector('#contentbody').addEventListener("webkitAnimationEnd", (e) => {
+            document.getElementById("lobby").classList.add("d-none");
+            document.getElementById("lobby").classList.remove("fadetoright");
+            document.querySelectorAll(".set1question")[current].classList.remove("d-none");
+            document.querySelectorAll(".set1question")[current].classList.add("fadefromleft1");
+        })
+    }
+    else if (e.target.classList.contains("set1ans") == true) {
+        console.log(current)
+
+        y = Array.from(document.querySelectorAll('.set1question')[current].getElementsByClassName("set1ans")).indexOf(e.target);
+        
+        if (current == 0) { if (y == 1) { cs = cs + 1; } }
+        else if (current == 1) { if (y == 2) { cs = cs + 1; } }
+        else if (current == 2) { if (y == 0) { cs = cs + 1; } }
+        else if (current == 3) { if (y == 1) { cs = cs + 1; } }
+        else if (current == 4) { if (y == 0) { cs = cs + 1; } }
+        else if (current == 5) { if (y == 2) { cs = cs + 1; } }
+        else if (current == 6) { if (y == 1) { cs = cs + 1; } }
+        else if (current == 7) { if (y == 3) { cs = cs + 1; } }
+        else if (current == 8) { if (y == 3) { cs = cs + 1; } }
+        else if (current == 9) { if (y == 0) { cs = cs + 1; } }
+
+        /* answer for set 2
+        if (current == 0) { if (y == 2) { cs = cs + 1; } }
+        else if (current == 1) { if (y == 2) { cs = cs + 1; } }
+        else if (current == 2) { if (y == 1) { cs = cs + 1; } }
+        else if (current == 3) { if (y == 2) { cs = cs + 1; } }
+        else if (current == 4) { if (y == 1) { cs = cs + 1; } }
+        else if (current == 5) { if (y == 3) { cs = cs + 1; } }
+        else if (current == 6) { if (y == 2) { cs = cs + 1; } }
+        else if (current == 7) { if (y == 0) { cs = cs + 1; } }
+        else if (current == 8) { if (y == 1) { cs = cs + 1; } }
+        else if (current == 9) { if (y == 1) { cs = cs + 1; } }
+        */
+        current = current + 1;
+        document.querySelectorAll(".set1question")[current - 1].classList.add("fadetoright");
+        document.querySelector('#contentbody').addEventListener("webkitAnimationEnd", (e) => {
+            document.querySelectorAll(".set1question")[current - 1].classList.add("d-none");
+            document.querySelectorAll(".set1question")[current].classList.remove("d-none");
+            document.querySelectorAll(".set1question")[current].classList.add("fadefromleft1");
+        })
+
+
+
+    }
+
+})
+
+
+function removeall(item, index) {
+    item.classList.add("d-none");
+    item.classList.remove("fadein");
+    item.classList.remove("fadefromleft2", "hidetillscroll", "fadefromleft1");
+}
