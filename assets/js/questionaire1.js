b83882ccca70b06695ebe455a0455b7f46aa705e
JJSamuel343 <joshuajs92@gmail.com>
2021-12-08 15:42:21 +0800

    test

--- assets/js/questionaire1.js
+++ assets/js/questionaire1.js
@@ -45,37 +45,56 @@ document.querySelector('#contentbody').addEventListener('click', (e) => {
         console.log(current)
 
         y = Array.from(document.querySelectorAll(`.${set}question`)[current].getElementsByClassName(`${set}ans`)).indexOf(e.target);
+        answers[current] = y;
 
-        if (set == "set1") {
-            if (current == 0) { if (y == 1) { cs = cs + 1; } }
-            else if (current == 1) { if (y == 2) { cs = cs + 1; } }
-            else if (current == 2) { if (y == 0) { cs = cs + 1; } }
-            else if (current == 3) { if (y == 1) { cs = cs + 1; } }
-            else if (current == 4) { if (y == 0) { cs = cs + 1; } }
-            else if (current == 5) { if (y == 2) { cs = cs + 1; } }
-            else if (current == 6) { if (y == 1) { cs = cs + 1; } }
-            else if (current == 7) { if (y == 3) { cs = cs + 1; } }
-            else if (current == 8) { if (y == 3) { cs = cs + 1; } }
-            else if (current == 9) { if (y == 0) { cs = cs + 1; } }
-        }
-        else {
-            if (current == 0) { if (y == 2) { cs = cs + 1; } }
-            else if (current == 1) { if (y == 2) { cs = cs + 1; } }
-            else if (current == 2) { if (y == 1) { cs = cs + 1; } }
-            else if (current == 3) { if (y == 2) { cs = cs + 1; } }
-            else if (current == 4) { if (y == 1) { cs = cs + 1; } }
-            else if (current == 5) { if (y == 3) { cs = cs + 1; } }
-            else if (current == 6) { if (y == 2) { cs = cs + 1; } }
-            else if (current == 7) { if (y == 0) { cs = cs + 1; } }
-            else if (current == 8) { if (y == 1) { cs = cs + 1; } }
-            else if (current == 9) { if (y == 1) { cs = cs + 1; } }
-        }
+        // if (set == "set1") {
+        //     if (current == 0) { if (y == 1) { cs = cs + 1; } }
+        //     else if (current == 1) { if (y == 2) { cs = cs + 1; } }
+        //     else if (current == 2) { if (y == 0) { cs = cs + 1; } }
+        //     else if (current == 3) { if (y == 1) { cs = cs + 1; } }
+        //     else if (current == 4) { if (y == 0) { cs = cs + 1; } }
+        //     else if (current == 5) { if (y == 2) { cs = cs + 1; } }
+        //     else if (current == 6) { if (y == 1) { cs = cs + 1; } }
+        //     else if (current == 7) { if (y == 3) { cs = cs + 1; } }
+        //     else if (current == 8) { if (y == 3) { cs = cs + 1; } }
+        //     else if (current == 9) { if (y == 0) { cs = cs + 1; } }
+        // }
+        // else {
+        //     if (current == 0) { if (y == 2) { cs = cs + 1; } }
+        //     else if (current == 1) { if (y == 2) { cs = cs + 1; } }
+        //     else if (current == 2) { if (y == 1) { cs = cs + 1; } }
+        //     else if (current == 3) { if (y == 2) { cs = cs + 1; } }
+        //     else if (current == 4) { if (y == 1) { cs = cs + 1; } }
+        //     else if (current == 5) { if (y == 3) { cs = cs + 1; } }
+        //     else if (current == 6) { if (y == 2) { cs = cs + 1; } }
+        //     else if (current == 7) { if (y == 0) { cs = cs + 1; } }
+        //     else if (current == 8) { if (y == 1) { cs = cs + 1; } }
+        //     else if (current == 9) { if (y == 1) { cs = cs + 1; } }
+        // }
         current = current + 1;
         document.querySelectorAll(`.${set}question`)[current - 1].classList.add("fadetoright");
         document.querySelector('#contentbody').addEventListener("webkitAnimationEnd", (e) => {
             document.querySelectorAll(`.${set}question`)[current - 1].classList.add("d-none");
             document.querySelectorAll(`.${set}question`)[current].classList.remove("d-none");
             document.querySelectorAll(`.${set}question`)[current].classList.add("fadefromleft1");
+
+            connect.POST(
+                "/assets/php/score.php",
+                {
+                    a:"SCORE",
+                    phone:document.getElementById("phonenumber").value,
+                    set : set,
+                    ans : answers 
+                },
+                (a,b) => {            
+                    if(b.status) {
+                        alert(`Thank you ! Your score is ${b.score} !`);
+                    } else {
+                        console.error(b);
+                    }
+    
+                }
+            )
         })
 
 
