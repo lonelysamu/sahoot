e7e33f28b59b7c05b21760685dbcf06852a04710
JJSamuel343 <joshuajs92@gmail.com>
2021-12-08 17:43:39 +0800

    test

--- assets/php/score.php
+++ assets/php/score.php
@@ -49,6 +49,7 @@ switch ($_POST['a']) {
             "set1" => [1, 2, 0, 1, 0, 2, 1, 3, 3, 0],
             "set2" => [2, 2, 1, 2, 1, 3, 2, 0, 1, 1]
         ];
+        $_POST['ans'] = explode(",",$_POST['ans']);
 
         for ($i = 0; $i < $max; $i++) {
             $score += ($_POST['ans'][$i] == $answers[$_POST['set']][$i]) ? 1 : 0;
